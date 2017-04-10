56090e2e41dbd1daf5d9911bbd6429f9b436b25e
bmwasaru <bmwasaru@gmail.com>
2017-04-11 00:18:27 +0300

    different local and heroku DATABASE_URL

--- mentor001/settings.py
+++ mentor001/settings.py
@@ -18,7 +18,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL')
+        default=config('LOCAL_DATABASE_URL')
     )
 }
 

4a0a88de630362ea37d2479b747522ffcd84dd5d
bmwasaru <bmwasaru@gmail.com>
2017-04-24 10:01:08 +0300

    some tests and initial reset password

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django_webtest import WebTest
 
-# Create your tests here.
+from authentication.models import Profile
+
+
+class ProfileCreateWhenUserIsCreated(WebTest):
+    """
+    Test that a user profile is create on user creation
+    """
+    def test_user_creation_creates_user_profile(self):
+        u = User.objects.create(username="bmwasaru")
+        u.save()
+        self.assert_(list(Profile.objects.filter(user__username="bmwasaru")))
+
+
+class Signup(WebTest):
+
+    def test_signup_redirect_to_settings(self):
+        signup_page = self.app.get('/signup/')
+        signup_form = signup_page.form
+        self.assertNotIn(
+            'User with this Username already exists.',
+            signup_form.text
+        )
+        signup_form['username'] = 'bmwasaru'
+        signup_form['email'] = 'bmwasaru@gmail.com'
+        signup_form['password'] = 'u937riehf44'
+        signup_form['confirm_password'] = 'u937riehf44'
+        response = signup_form.submit()
+        self.assertIn('/settings/', response.location)
--- mentor001/urls.py
+++ mentor001/urls.py
@@ -4,7 +4,6 @@ from django.contrib.auth import views as auth_views
 
 from core import views as core_views
 from authentication import views as mentor_auth_views
-from mentorship import views as mentoship_views
 from activities import views as activities_views
 
 urlpatterns = [
@@ -17,6 +16,26 @@ urlpatterns = [
         name='login'),
     url(r'^logout', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^signup/$', mentor_auth_views.signup, name='signup'),
+    url(r'^forgot_pass/$',
+        auth_views.password_reset, {
+            'template_name': 'authentication/password_reset.html',
+            'email_template_name': 'authentication/password_reset_email.html'
+        },
+        "forgot_password"),
+    url(r'^forgot_pass_confirm/(?P<uidb64>[^/]+)/(?P<token>[^/]+)/$',
+        auth_views.password_reset_confirm,
+        {'template_name': 'authentication/password_reset_confirm.html',
+         'post_reset_redirect': '/account/forgot_pass_complete/'}, name='password_reset_confirm'),
+
+    url(r'^forgot_pass_done/', auth_views.password_reset_done,
+        {'template_name': 'authentication/password_reset_done.html'}, name='password_reset_done'),
+
+    url(r'^account/forgot_pass_complete/', auth_views.password_reset_complete,
+        {'template_name': 'authentication/password_reset_complete.html'}),
+
+    url(r'^forgot_pass_confirm/(?P<uidb64>[^/]+)/(?P<token>[^/]+)/$',
+        auth_views.password_reset_confirm,
+        {'template_name': 'authentication/password_reset_confirm.html'}, name='password_reset_confirm'),
     url(r'^settings/$', core_views.settings, name='settings'),
     url(r'^settings/password/$', core_views.password, name='password'),
     url(r'^questions/', include('questions.urls'), name='questions'),
@@ -27,8 +46,6 @@ urlpatterns = [
         name='last_notifications'),
     url(r'^notifications/check/$', activities_views.check_notifications,
         name='check_notifications'),
-    # Mentorship URLs
-    url(r'request/mentor/(?P<mentor_id>[0-9]+)/$', mentoship_views.request_mentorship, name='request-mentorship'),
     url(r'^(?P<username>[^/]+)/$', core_views.profile, name='profile'),
 ]
 

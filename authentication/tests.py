2862e10147e6860a7deb25f5cb59e282b1fea20b
bmwasaru <bmwasaru@gmail.com>
2017-06-20 18:59:13 +0300

    tests failing :(

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,17 +1,10 @@
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.test import Client
+
 from django_webtest import WebTest
 
 from authentication.models import Profile
-
-
-class ProfileCreateWhenUserIsCreated(WebTest):
-    """
-    Test that a user profile is create on user creation
-    """
-    def test_user_creation_creates_user_profile(self):
-        u = User.objects.create(username="bmwasaru")
-        u.save()
-        self.assert_(list(Profile.objects.filter(user__username="bmwasaru")))
 
 
 class Signup(WebTest):
@@ -28,4 +21,42 @@ class Signup(WebTest):
         signup_form['password'] = 'u937riehf44'
         signup_form['confirm_password'] = 'u937riehf44'
         response = signup_form.submit()
+        self.assertEqual(response.status_code, 200)
         self.assertIn('/settings/', response.location)
+
+
+class Login(WebTest):
+    """Tests for user login and logout"""
+
+    def setUp(self):
+        User.objects.create(
+            username="bmwasaru", password="u937riehf44", is_active=True)
+
+    def test_login(self):
+        user = authenticate(username="bmwasaru", password="u937riehf44")
+        self.assertTrue(user and user.is_active)
+
+    def test_logout(self):
+        self.client = Client()
+        username = "bmwasaru"
+        password = "u937riehf44"
+        self.client.login(username=username, password=password)
+        # check that the user is logged in
+        self.assertEqual(1, self.client.session.get('_auth_user_id'))
+
+        # logout the user
+        self.client.logout()
+        self.assertNotEqual(1, self.client.session.get('_auth_user_id'))
+        questions_page = self.app.get('/questions/')
+        self.assertIn('Log in', questions_page.content)
+
+
+class ProfileCreateWhenUserIsCreated(WebTest):
+    """
+    Test that a user profile is create on user creation
+    """
+
+    def test_user_creation_creates_user_profile(self):
+        u = User.objects.create(username="bmwasaru")
+        u.save()
+        self.assert_(list(Profile.objects.filter(user__username="bmwasaru")))

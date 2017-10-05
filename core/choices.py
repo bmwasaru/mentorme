0c53a9d3abf65227d086054a525a2aaed0f89642
Britone Mwasaru <bmwasaru@gmail.com>
2017-10-05 18:55:02 +0300

    Spelling errors

--- core/choices.py
+++ core/choices.py
@@ -1,26 +1,26 @@
 EDUCATION_CHOICES = (('primary', 'Primary'),
-					('secondary', 'Secondary'),
-					('certificate', 'Certificate'),
-					('diploma', 'Diploma'),
-					('undergraduate', 'Undergraduate'),
-					('postgraduate', 'Postgraduate'))
+                     ('secondary', 'Secondary'),
+                     ('certificate', 'Certificate'),
+                     ('diploma', 'Diploma'),
+                     ('undergraduate', 'Undergraduate'),
+                     ('postgraduate', 'Postgraduate'))
 
 MENTORSHIP_AREAS_CHOICES = (
-	('academic_assistance', 'Academic Assistance'),
-	('entrepreneurship', 'Entrepreneurship (Having difficulties being your own boss?)'),  
-	('career_counceling', 'Career Counceling'),
-	('career_readiness', 'Career readiness (Interviews or Job hunting)'),
-	('grief_loss', 'Coping with grief and loss'),
-	('addictions', 'Coping with addictions'),
-	('abuse', 'Trauma and Abuse'),
-	('no_preference', 'Prefere not to answer'),)
+    ('academic_assistance', 'Academic Assistance'),
+    ('entrepreneurship', 'Entrepreneurship (Having difficulties being your own boss?)'),
+    ('career_counseling', 'Career Counseling'),
+    ('career_readiness', 'Career readiness (Interviews or Job hunting)'),
+    ('grief_loss', 'Coping with grief and loss'),
+    ('addictions', 'Coping with addictions'),
+    ('abuse', 'Trauma and Abuse'),
+    ('no_preference', 'Prefer not to answer'),)
 
 CATEGORY_CHOICES = (
-	('tech', 'Tech'),
-	('arts', 'Arts'),
-	('programming', 'Programming'),
-	('web_development', 'Web Development'),
-	('actor', 'Actor'),
-	('actress', 'Actress'),
-	('law', 'Law'),
-	('medicine', 'Medicine'))
+    ('tech', 'Tech'),
+    ('arts', 'Arts'),
+    ('programming', 'Programming'),
+    ('web_development', 'Web Development'),
+    ('actor', 'Actor'),
+    ('actress', 'Actress'),
+    ('law', 'Law'),
+    ('medicine', 'Medicine'))

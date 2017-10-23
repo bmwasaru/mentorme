c061a6f63e20b9df64355c73390af4d21eb6ecb3
Britone Mwasaru <bmwasaru@gmail.com>
2017-10-23 12:00:24 +0300

    PEP8 stuff

--- authentication/tokens.py
+++ authentication/tokens.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils import six
 
+
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (

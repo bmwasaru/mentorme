83bf5602184acfcf77b15ef4754356d05893c0ba
Britone Mwasaru <bmwasaru@gmail.com>
2020-02-04 14:29:53 +0300

    Django import changes

--- authentication/views.py
+++ authentication/views.py
@@ -2,14 +2,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 
-from django.contrib.auth.decorators import login_required
 from authentication.forms import SignUpForm
-
-from django.contrib.sites.shortcuts import get_current_site
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
-from django.template.loader import render_to_string
-from authentication.tokens import account_activation_token
 
 
 def signup(request):
@@ -30,24 +23,3 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'authentication/signup.html', {'form': form})
-
-
-def account_activation_sent(request):
-    return render(request, 'authentication/account_activation_sent.html')
-
-
-def activate(request, uidb64, token):
-    try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
-
-    if user is not None and account_activation_token.check_token(user, token):
-        user.is_active = True
-        user.profile.email_confirmed = True
-        user.save()
-        login(request, user)
-        return redirect('/')
-    else:
-        return render(request, 'authentication/account_activation_invalid.html')

a47a27b7895e2771cd5fb680bf65afa00d9238d2
Britone Mwasaru <bmwasaru@gmail.com>
2022-10-17 11:31:15 +0300

    Update settings.py

--- mentorme/settings.py
+++ mentorme/settings.py
@@ -10,7 +10,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'B7Kid53HcUmeqVEmTFs5RIfmnaU4aTA2gxV4b4J9'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
